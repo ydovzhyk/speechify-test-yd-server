3a88da19406db9279fec4f39b5ee846d0c9a1ce3
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-07-24 14:23:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get("*", (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
